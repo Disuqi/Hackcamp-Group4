f4b93b1c7e417458a16aeece9d0564a7f0b5890a
Disuqi <disuqi.hijazi.7@gmail.com>
2022-01-21 13:00:30 +0000

    small change to improve speed

--- controllers/index.php
+++ controllers/index.php
@@ -10,12 +10,5 @@ if(isset($_POST['mainSearch']) && trim($_POST['mainSearch']) != ""){
     $dataSet = new ItemDataSet();
     $view->items = $dataSet->search($_POST['mainSearch']);
 }
-if(sizeof($view->items) == 0) {
-    if(!isset($_SESSION['jsonFile'])) {
-        $json = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/json/data.json');
-        $_SESSION['jsonFile'] = json_decode($json, true);
-    }
-    $view->items = $_SESSION['jsonFile'];
-}
 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/views/index.phtml";
