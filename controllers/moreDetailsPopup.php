3f6f29d5d1edae85a4a52b8fca3c4d8f9bf0dbfd
touqeerahmed111 <69014269+touqeerahmed111@users.noreply.github.com>
2022-01-22 05:55:10 +0000

    added more details popup page

--- controllers/moreDetailsPopup.php
+++ controllers/moreDetailsPopup.php
@@ -1,21 +1,12 @@
 <?php
 $view = new stdClass();
-$view->items = [];
-$view->fields = ['appid', 'release_date', 'english', 'developer', 'publisher', 'status', 'platforms', 'required_age', 'categories', 'genres', 'tags', 'achievements', 'positive_ratings', 'negative_ratings', 'average_playtime', 'median_playtime', 'physical', 'units_available', 'units_sold', 'price'];
-$view->defaultFields = ['appid', 'release_date', 'developer', 'status'];
 
 require_once  $_SERVER['DOCUMENT_ROOT'] . "/models/itemDataSet.php";
-
-if(isset($_POST['mainSearch']) && trim($_POST['mainSearch']) != ""){
-    $dataSet = new ItemDataSet();
-    $view->items = $dataSet->search($_POST['mainSearch']);
-}
 
 if(isset($_POST['checking-view-btn'])){
     $appid = $_POST['app-id'];
     $app_id = (int) filter_var($appid, FILTER_SANITIZE_NUMBER_INT);
-//    echo $return = $app_id;
-//    die();
+
     $dataSet = new ItemDataSet();
     $sqlQuery = "SELECT * FROM gamesales WHERE appid = $app_id";
     $view->items = $dataSet->getObjectsFromQuery($sqlQuery);
@@ -33,7 +24,6 @@ if(isset($_POST['checking-view-btn'])){
             <h5><span style="font-weight: bold">Status: </span>'.$item->getStatus().' </h5>
             <h5><span style="font-weight: bold">Platforms: </span>'.$platforms.' </h5>
             <h5><span style="font-weight: bold">Required Age: </span>'.$item->getRequiredAge().' </h5>
-            
             <h5><span style="font-weight: bold">Categories: </span>'.$categories.' </h5>
             <h5><span style="font-weight: bold">Genres: </span>'.$genres.' </h5>
             <h5><span style="font-weight: bold">Tags: </span>'.$tags.' </h5>
@@ -52,6 +42,3 @@ if(isset($_POST['checking-view-btn'])){
     }
 
 }
-
-
-//require_once $_SERVER['DOCUMENT_ROOT'] . "/views/template/footer.phtml";
