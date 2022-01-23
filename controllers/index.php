4b78e5ef860139e955e60cfa7c9b9d0fd3279935
Disuqi <disuqi.hijazi.7@gmail.com>
2022-01-23 11:37:05 +0000

    cards will show when doing a search

--- controllers/index.php
+++ controllers/index.php
@@ -3,6 +3,9 @@ $view = new stdClass();
 $view->items = [];
 $view->fields = ['appid', 'release_date', 'english', 'developer', 'publisher', 'status', 'platforms', 'required_age', 'categories', 'genres', 'tags', 'achievements', 'positive_ratings', 'negative_ratings', 'average_playtime', 'median_playtime', 'physical', 'units_available', 'units_sold', 'price'];
 $view->defaultFields = ['appid', 'release_date', 'developer', 'status'];
+if(session_id() == ""){
+    session_start();
+}
 
 require_once  $_SERVER['DOCUMENT_ROOT'] . "/models/itemDataSet.php";
 
@@ -10,5 +13,7 @@ if(isset($_POST['mainSearch']) && trim($_POST['mainSearch']) != ""){
     $dataSet = new ItemDataSet();
     $view->items = $dataSet->search($_POST['mainSearch']);
 }
-
+if(isset($_GET['cards'])){
+    $_SESSION['cards'] = $_GET['cards'];
+}
 require_once $_SERVER['DOCUMENT_ROOT'] . "/views/index.phtml";
