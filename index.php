400fdcf08befcfac21448f60ab5cc7d4256b9a5a
Beriatan <61006198+Beriatan@users.noreply.github.com>
2022-01-24 15:39:05 +0000

    left panel navigation fixed

--- index.php
+++ index.php
@@ -16,4 +16,27 @@ if(isset($_POST['mainSearch']) && trim($_POST['mainSearch']) != ""){
 if(isset($_GET['cards'])){
     $_SESSION['cards'] = $_GET['cards'];
 }
+
+
+if(isset($_GET['platforms'])){
+    $dataSet = new ItemDataSet();
+    $view->items = $dataSet->search("platforms:" .$_GET['platforms']);
+}
+if(isset($_GET['developer'])){
+    $dataSet = new ItemDataSet();
+    $view->items = $dataSet->search("developer:" .$_GET['developer']);
+}
+if(isset($_GET['status'])){
+    $dataSet = new ItemDataSet();
+    $view->items = $dataSet->search("status:" .$_GET['status']);
+}
+if(isset($_GET['categories'])){
+    $dataSet = new ItemDataSet();
+    $view->items = $dataSet->search("categories:" .$_GET['categories']);
+}
+if(isset($_GET['tags'])){
+    $dataSet = new ItemDataSet();
+    $view->items = $dataSet->search("tags:" .$_GET['tags']);
+}
+
 require_once('views/index.phtml');
